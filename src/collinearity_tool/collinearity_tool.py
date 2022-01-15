2b6c9134a8a50cb5593a8051556ce392b39c9c14
nickmao <nickmao1994@gmail.com>
2022-01-14 16:40:57 -0800

    Update details after discussion

--- src/collinearity_tool/collinearity_tool.py
+++ src/collinearity_tool/collinearity_tool.py
@@ -1,18 +1,22 @@
 def corr_matrix(df):
-    """Select all numeric variables and calculate 
-    Pearson correlation coefficient pairwise.
+    """Select all numeric variables and calculate
+    Pearson correlation coefficient pairwise. The output
+    of this function has all numeric variables as
+    columns and rows and correlation coefficient
+    as each data point.
     
     Parameters
     ----------
-    df : The input data frame.
+    df : pandas.DataFrame 
+        The input data frame.
 
     Returns
     -------
-    pandas.dataframe
+    pandas.DataFrame
         A correlation matrix.
     
     Examples
     --------
-    >>> from collinearity_tool.corr_matrix import corr_matrix
+    >>> from collinearity_tool.collinearity_tool import corr_matrix
     >>> corr_df = corr_matrix(df)
     """
