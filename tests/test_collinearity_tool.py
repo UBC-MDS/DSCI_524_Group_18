613f9c4ba9addf6ab34d975542e175e2a7a620c5
Katia Aristova <k.aristova@gmail.com>
2022-01-28 13:05:10 -0800

    Fix test file with col_identify

--- tests/test_collinearity_tool.py
+++ tests/test_collinearity_tool.py
@@ -134,3 +134,39 @@ def test_vif_bar_plot():
     assert vif_scores_and_plot[1].layer[1].mark.color == "red", "The threshold should be a red line."
     assert vif_scores_and_plot[1].layer[1].mark.type == "rule", "The threshold should be a line spanning the axis."
     assert vif_scores_and_plot[1] == vif_scores_and_plot[1].layer[0] + vif_scores_and_plot[1].layer[1], "The plot should be a layered plot (bar + line)."
+
+
+def test_col_identify():
+    """
+    Tests outputs and other parameters of the col_identify()
+    function.
+    
+    Examples
+    --------
+    >>> test_col_identify()
+    """
+
+    cars = pd.read_csv("https://raw.githubusercontent.com/tidyverse/ggplot2/main/data-raw/mpg.csv")
+    exp_list = ["displ", "year", "cyl", "cty"]
+    resp_y = "hwy"
+
+    col_df = col_identify(cars, exp_list, resp_y)
+
+    # output and its properties
+    assert type(col_df) == pd.DataFrame, "The output should be a Pandas DataFrame"
+    assert col_df.shape[1] == 6, "The output should contain 6 columns"
+    assert col_df.columns.to_list() == ["variable", "pair", "correlation", "rounded_corr",
+                             "vif_score", "eliminate"], "The following columns should be produced: variable, pair, correlation, rounded_corr, vif_score, eliminate"
+
+    col_df = round(col_df, 4)
+
+    data = {"variable": ["cyl", "cty", "cty", "displ", "cyl"],
+        "pair": ["cty | cyl", "cty | cyl", "cty | hwy", "cyl | displ", "cyl | displ"],
+        "correlation": [-0.8058, -0.8058, 0.9559, 0.9302, 0.9302],
+        "rounded_corr": [-0.81, -0.81, 0.96, 0.93, 0.93],
+        "vif_score": [8.0817, 3.0547, 3.0547, 7.9384, 8.0817],
+        "eliminate": ["Yes", "No", "No", "Yes", "Yes"]}
+
+    test_df = round(pd.DataFrame(data), 4)
+
+    assert col_df.equals(test_df), "The test and real dataframes should be the same"
